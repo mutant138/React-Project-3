77216bdb3fe16639e57aeb4a474fa6c70ce16692
mutant138 <udhayasurya138@gmail.com>
2024-06-18 23:22:26 +0530

    meals_component_done

---
+++ ReactRestaurant/src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import classes from './Header.module.css'
+import mealsImg from '../../assests/meals.jpg'
+import HeaderCartButton from "./HeaderCartButton"
+
+const Header = props =>{
+ return(
+ <>
+ <header className={classes.header}>
+  <h1>React Restaurant</h1>
+  <HeaderCartButton></HeaderCartButton>
+ </header>
+ <div className={classes['main-image']}> 
+ <img src={mealsImg} alt="Delicious food"></img>
+ </div>
+ </>
+ )
+ //main-image use square notation if classes with - 
+}
+
+
+
+export default Header

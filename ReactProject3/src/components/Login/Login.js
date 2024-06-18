69a421b4bca30da7a55f203bbb02689ac0420d89
mutant138 <udhayasurya138@gmail.com>
2024-06-18 22:37:57 +0530

    Restaurant_project

--- ReactProject3/src/components/Login/Login.js
+++ ReactProject3/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState , useEffect , useReducer } from 'react';
-
+import React, { useState , useEffect , useReducer , useContext, useRef } from 'react';
+import Input from '../UI/Input/input';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state,action)=>{
   if(action.type==="USER_INPUT"){
@@ -33,20 +34,25 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
   const [emailState, dispatchEmail] = useReducer(emailReducer , {value: "", isValid: null})
   const [passwordState, dispatchPassword] = useReducer(passwordReducer , {value: "", isValid: null})
+  const authCtx = useContext(AuthContext)
 
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
+  const {isValid: emailIsValid} = emailState
+  const {isValid: passwordIsValid} = passwordState
   useEffect(()=>{
     
     const identifier = setTimeout(() => {
       console.log("Checking Form Validity..!")
       setFormIsValid(
-        emailState.isValid && passwordState.isValid
+        emailIsValid && passwordIsValid
       );
     }, 500);
     return ()=>{
       console.log("Cleanup function");  //It will prevent sending unnecessary http req used in timeout
       clearTimeout(identifier)
     }
-  }, [passwordState.isValid , emailState.isValid ])
+  }, [emailIsValid , passwordIsValid ])
 
 
   const emailChangeHandler = (event) => {
@@ -67,42 +73,40 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    if(formIsValid){
+      authCtx.onLogin(emailState.value, passwordState.value);
+    }else if(!emailIsValid){
+    emailInputRef.current.activate()
+    }else{
+    passwordInputRef.current.activate()
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+        ref={emailInputRef}
+        id="email"
+        label="E-Mail" 
+        type="email" 
+        isValid={emailIsValid} 
+        value = {emailState.value} 
+        onChange={emailChangeHandler}
+        onBlur = {validateEmailHandler}
+        />
+        <Input
+        ref={passwordInputRef}
+        id="password"
+        label="Password" 
+        type="password" 
+        isValid={passwordIsValid} 
+        value = {passwordState.value} 
+        onChange={passwordChangeHandler}
+        onBlur = {validatePasswordHandler}
+        />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>

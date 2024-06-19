b41d52d8b7f9782675dc8afbe0afc7826aaf8c01
mutant138 <udhayasurya138@gmail.com>
2024-06-19 13:07:46 +0530

    cartitem_badge_fixed

--- ReactRestaurant/src/components/Layout/HeaderCartButton.js
+++ ReactRestaurant/src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,15 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 
 const HeaderCartButton = props =>{
+    
+const ctx = useContext(CartContext)
+const numOfItems = ctx.items.reduce((acc,curr)=>{
+    return acc + curr.amount
+},0)
     return(
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -12,7 +19,7 @@ const HeaderCartButton = props =>{
                 Your Cart
             </span>
             <span className={classes.badge}>
-                0
+                {numOfItems}
             </span>
         </button>
     )
